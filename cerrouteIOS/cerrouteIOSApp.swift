1a46f82fd664aba1ebdde33d0d83b2e495136aac
melikbilyay <37048509+melikbilyay@users.noreply.github.com>
2024-12-01 19:45:26 +0300

    Initial Commit

---
+++ cerrouteIOS/cerrouteIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  cerrouteIOSApp.swift
+//  cerrouteIOS
+//
+//  Created by Melik Bilyay on 1.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct cerrouteIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
